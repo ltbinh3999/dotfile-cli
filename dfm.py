fa18c56cbe68bcb168669df074b51acfc8e8321e
Thanh Binh Le <ltbinh3999@gmail.com>
2021-03-26 09:49:19 +0700

    feat: link command

--- dfm.py
+++ dfm.py
@@ -2,11 +2,15 @@ import click
 import pickle
 import os
 from click.utils import echo
+import sys
+from subprocess import Popen
+
+DATABASE = os.path.expanduser("~/.dfm_data")
 
 
 def load_data():
     try:
-        with open(".data", "rb+") as f:
+        with open(f"{DATABASE}/.data", "rb+") as f:
             data = pickle.load(f)
     except OSError:
         data = []
@@ -14,21 +18,34 @@ def load_data():
 
 
 def write_data(data):
-    pickle.dump(data, open(".data", "wb"))
+    pickle.dump(data, open(f"{DATABASE}/.data", "wb"))
+
+
+@click.command()
+def link():
+    data = load_data()
+    for src, dest in data:
+        dest = f"{DATABASE}/{dest}.config"
+        os.symlink(dest, src)
 
 
 @click.command()
 @click.argument("src", type=click.Path())
 @click.argument("name")
 def add(src, name):
-    """Add file SRC to database as NAME"""
+    """Add SRC to data as NAME"""
     data = load_data()
     if src in {i[0] for i in data}:
         raise click.BadParameter(src, param_hint="SRC")
     if name in {i[1] for i in data}:
         raise click.BadParameter(name, param_hint="NAME")
     try:
-        dest = f"{os.getcwd()}/{name}.config"
+        try:
+            os.mkdir(DATABASE)
+            Popen(["git", "init"], cwd=DATABASE)
+        except FileExistsError:
+            pass
+        dest = f"{DATABASE}/{name}.config"
         os.replace(src, dest)
         os.symlink(dest, src)
         data.append((src, name))
@@ -41,14 +58,14 @@ def add(src, name):
 @click.command()
 @click.argument("name")
 def delete(name):
-    """Delete NAME from database"""
+    """Delete NAME from data"""
     data = load_data()
     for k, v in enumerate(data):
         if v[1] == name:
             data.pop(k)
             write_data(data)
             try:
-                dest = f"{os.getcwd()}/{v[1]}.config"
+                dest = f"{DATABASE}/{v[1]}.config"
                 os.replace(dest, v[0])
             except FileNotFoundError:
                 pass
@@ -71,7 +88,7 @@ def show():
 @click.command()
 def test():
     """DEBUG METHOD"""
-    raise click.UsageError("bullshit")
+    echo(sys.path[0])
 
 
 @click.group()
@@ -83,5 +100,6 @@ dfm.add_command(add)
 dfm.add_command(show)
 dfm.add_command(test)
 dfm.add_command(delete)
+dfm.add_command(link)
 if __name__ == "__main__":
     dfm()
